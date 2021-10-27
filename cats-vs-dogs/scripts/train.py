bfbf02f42f24ae5ec04bf13a92e220254f8d5f50
jyuteo <jyuteo19@outlook.com>
2021-10-27 16:25:05 +0800

    Update training script

--- cats-vs-dogs/scripts/train.py
+++ cats-vs-dogs/scripts/train.py
@@ -5,15 +5,21 @@ import time
 import random
 import argparse
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 from tqdm import tqdm
 from torch.optim import lr_scheduler
 from torchvision import datasets, transforms
-from models.resnet import resnet50
+from models.resnet import resnet50, resnet18
+
+# from dataloader import get_dataloader
 
 IMAGE_SIZE = (224, 224)
 
@@ -27,10 +33,50 @@ def set_seed(seed):
     random.seed(seed)
 
 
+def plot_graphs(output_dir, num_epochs):
+    log_df = pd.read_csv(os.path.join(output_dir, "log.csv"))
+
+    fig = plt.gcf()
+    fig.set_size_inches(15, 5)
+
+    plt.subplot(1, 2, 1)
+    plt.title("Loss")
+    plt.plot(np.arange(1, num_epochs + 1, 1), log_df["train_loss"], color="tab:blue")
+    plt.plot(np.arange(1, num_epochs + 1, 1), log_df["val_loss"], color="tab:orange")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    custom_lines = [
+        Line2D([0], [0], color="tab:blue", lw=2),
+        Line2D([0], [0], color="tab:orange", lw=2),
+    ]
+    plt.legend(custom_lines, ["train", "val"])
+
+    plt.subplot(1, 2, 2)
+    plt.title("Accuracy")
+    plt.plot(np.arange(1, num_epochs + 1, 1), log_df["train_acc"], color="tab:blue")
+    plt.plot(np.arange(1, num_epochs + 1, 1), log_df["val_acc"], color="tab:orange")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    custom_lines = [
+        Line2D([0], [0], color="tab:blue", lw=2),
+        Line2D([0], [0], color="tab:orange", lw=2),
+    ]
+    plt.legend(custom_lines, ["train", "val"])
+
+    plt.savefig(os.path.join(output_dir, "loss_acc.png"))
+
+
 def get_model(model_name):
     if model_name == "resnet50":
-        model = resnet50(pretrained=True)
-        model.fc = nn.Linear(in_features=2048, out_features=2, bias=True)
+        model = resnet50()
+        model.fc = nn.Sequential(
+            nn.Linear(in_features=2048, out_features=2, bias=True), nn.Softmax(dim=1)
+        )
+    elif model_name == "resnet18":
+        model = resnet18()
+        model.fc = nn.Sequential(
+            nn.Linear(in_features=512, out_features=2, bias=True), nn.Softmax(dim=1)
+        )
     else:
         raise Exception("Invalid model name")
     return model
@@ -50,16 +96,10 @@ def get_transformations(augment):
         )
     else:
         transformations["train"] = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Resize(IMAGE_SIZE),
-            ]
+            [transforms.ToTensor(), transforms.Resize(IMAGE_SIZE),]
         )
     transformations["val"] = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Resize(IMAGE_SIZE),
-        ]
+        [transforms.ToTensor(), transforms.Resize(IMAGE_SIZE),]
     )
     return transformations
 
@@ -69,15 +109,16 @@ def train_step(batch_images, batch_labels, model, criterion, optimizer, device):
     labels = batch_labels.to(device)
     optimizer.zero_grad()
 
-    with torch.set_grad_enabled(mode=True):
+    with torch.enable_grad():
         output = model(batch_images)
-        _, preds = torch.max(output, 1)
+        preds = torch.argmax(output, dim=1)
         loss = criterion(output, labels)
+        batch_loss = loss.item()
+        batch_corrects = torch.sum(preds == labels).item()
+        loss = loss / labels.size(0)
         loss.backward()
         optimizer.step()
 
-    batch_loss = (loss * batch_images.size(0)).item()
-    batch_corrects = torch.sum(preds == labels.data).item()
     return batch_loss, batch_corrects
 
 
@@ -85,19 +126,18 @@ def val_step(batch_images, batch_labels, model, criterion, device):
     batch_images = batch_images.to(device)
     labels = batch_labels.to(device)
 
-    with torch.set_grad_enabled(False):
+    with torch.no_grad():
         output = model(batch_images)
-        _, preds = torch.max(output, 1)
+        preds = torch.argmax(output, dim=1)
+        batch_corrects = torch.sum(preds == labels).item()
         loss = criterion(output, labels)
+        batch_loss = loss.item()
 
-    batch_loss = (loss * batch_images.size(0)).item()
-    batch_corrects = torch.sum(preds == labels.data).item()
     return batch_loss, batch_corrects
 
 
 def train(
     model,
-    image_datasets,
     dataloaders,
     criterion,
     optimizer,
@@ -114,7 +154,7 @@ def train(
     # open log file
     log_file_path = os.path.join(output_dir, "log.csv")
     with open(log_file_path, "w") as f:
-        f.write("epoch,train_loss,train_acc,val_loss,val_acc\n")
+        f.write("epoch,train_loss,train_acc,val_loss,val_acc,learning_rate\n")
 
     for epoch in range(num_epochs):
         print("Epoch {}/{}".format(epoch + 1, num_epochs))
@@ -124,44 +164,74 @@ def train(
 
         train_running_loss = 0.0
         train_corrects = 0
+        cur_count = 0
 
-        for batch_images, batch_labels in tqdm(dataloaders["train"]):
+        pbar = tqdm(dataloaders["train"])
+
+        for batch_images, batch_labels in pbar:
             batch_loss, batch_corrects = train_step(
                 batch_images, batch_labels, model, criterion, optimizer, device
             )
             train_running_loss += batch_loss
             train_corrects += batch_corrects
-            lr_scheduler.step()
+            cur_count += batch_labels.size(0)
+            pbar.set_postfix(
+                {
+                    "loss": train_running_loss / cur_count,
+                    "acc": float(train_corrects) / cur_count,
+                }
+            )
 
-        train_loss = train_running_loss / len(image_datasets["train"])
-        train_acc = float(train_corrects) / len(image_datasets["train"])
+        train_loss = train_running_loss / cur_count
+        train_acc = float(train_corrects) / cur_count
 
         # val loop
         model.eval()
 
         val_running_loss = 0.0
         val_corrects = 0
+        cur_count = 0
 
-        for batch_images, batch_labels in tqdm(dataloaders["val"]):
+        pbar = tqdm(dataloaders["val"])
+
+        for batch_images, batch_labels in pbar:
             batch_loss, batch_corrects = val_step(
                 batch_images, batch_labels, model, criterion, device
             )
             val_running_loss += batch_loss
             val_corrects += batch_corrects
+            cur_count += batch_labels.size(0)
+            pbar.set_postfix(
+                {
+                    "loss": val_running_loss / cur_count,
+                    "acc": float(val_corrects) / cur_count,
+                }
+            )
 
-        val_loss = val_running_loss / len(image_datasets["val"])
-        val_acc = float(val_corrects) / len(image_datasets["val"])
+        val_loss = val_running_loss / cur_count
+        val_acc = float(val_corrects) / cur_count
 
         # log
         with open(log_file_path, "a") as f:
             f.write(
-                "{},{:.5f},{:.4f},{:.5f},{:.4f}\n".format(
-                    epoch + 1, train_loss, train_acc, val_loss, val_acc
+                "{},{:.5f},{:.4f},{:.5f},{:.4f},{:.6f}\n".format(
+                    epoch + 1,
+                    train_loss,
+                    train_acc,
+                    val_loss,
+                    val_acc,
+                    lr_scheduler.get_last_lr()[0],
                 )
             )
 
-        print("Train Loss: {:.5f} Acc: {:.4f}".format(train_loss, train_acc))
-        print("Valid Loss: {:.5f} Acc: {:.4f}".format(val_loss, val_acc))
+        print(
+            "Train Loss: {:.5f} Acc: {:.4f} \t Valid Loss: {:.5f} Acc: {:.4f} \t Base LR: {:.6f}".format(
+                train_loss, train_acc, val_loss, val_acc, lr_scheduler.get_last_lr()[0]
+            )
+        )
+        # print("Valid Loss: {:.5f} Acc: {:.4f}".format(val_loss, val_acc))
+        # print("Base LR: {:.6f}".format(lr_scheduler.get_last_lr()[0]))
+        lr_scheduler.step()
 
         if val_acc > best_acc:
             best_acc = val_acc
@@ -230,13 +300,12 @@ def main(args):
 
     lr = args.lr
     num_epochs = args.num_epochs
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss(reduction="sum")
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
 
     trained_model = train(
         model,
-        image_datasets,
         dataloaders,
         criterion,
         optimizer,
@@ -247,6 +316,8 @@ def main(args):
     )
 
     torch.save(trained_model, os.path.join(output_dir, "model.pt"))
+
+    plot_graphs(output_dir, num_epochs)
 
 
 if __name__ == "__main__":
@@ -263,10 +334,10 @@ if __name__ == "__main__":
         default="../models/",
         help="dir for saving trained models",
     )
-    parser.add_argument("--num_epochs", type=int, default=10)
+    parser.add_argument("--num_epochs", type=int, default=120)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--num_workers", type=int, default=4)
-    parser.add_argument("--lr", type=float, default=0.001, help="learning_rate")
+    parser.add_argument("--lr", type=float, default=0.0001, help="learning_rate")
     parser.add_argument(
         "--augment",
         default=False,
